eac8a950104744c6b67fc5906cd5b41cf3e23dd7
pelumi <ppeliance@gmail.com>
2024-11-24 23:58:47 -0800

    new project

--- src/container/project/project.js
+++ src/container/project/project.js
@@ -27,6 +27,12 @@ export default function Project() {
             text: "A Real Estate website that showcases properties for sale",
             image: "assets/estate.jpg"
         }
+        {
+            github: "https://github.com/pellypepper/Django",
+            web: "http://pellysticky-35c9341f2838.herokuapp.com",
+            text: "A Notepad App using Django",
+            image: "assets/notepad.jpg"
+        }
     ];
 
     return (
